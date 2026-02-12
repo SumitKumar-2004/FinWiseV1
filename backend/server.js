91b1bc42853a17927b149fec6a952bda896f10f3
SUMIT KUMAR <125842926+SumitKumar-2004@users.noreply.github.com>
2026-02-12 23:14:31 +0530

    Fix database connection and update server port

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,12 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config();
-const connectDB = require("./config/db.js");
+const connectDB = require("./config/db");
 
-
-//Database connection
 connectDB();
 
-const port = process.env.PORT || 8001;
-const server = app.listen(port, () => {
-  console.log("Server is running on port", port);
-});
+const PORT = process.env.PORT || 8000;
 
-process.on("SIGINT", async () => {
-  await mongoose.connection.close();
-  server.close(() => {
-    console.log("Server Stopped");
-    process.exit(0);
-  });
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
-
